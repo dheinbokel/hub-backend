f87d233cc694822d297688571f37787c9a2e8abf
Doug Heinbokel <dheinbokel@student.ncmich.edu>
2019-02-21 00:51:02 -0500

    Fixed Content by adding a getter and setter for the hubUsers field.  Now Content sends back the appropriate information.

--- src/main/java/com/hub/models/Content.java
+++ src/main/java/com/hub/models/Content.java
@@ -107,4 +107,12 @@ public class Content {
     public void setTags(Set<Tag> tags) {
         this.tags = tags;
     }
+
+    public Set<HubUser> getHubUsers() {
+        return hubUsers;
+    }
+
+    public void setHubUsers(Set<HubUser> hubUsers) {
+        this.hubUsers = hubUsers;
+    }
 }
